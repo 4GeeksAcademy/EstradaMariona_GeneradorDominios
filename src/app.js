7e27aaa2f789bc238147f142829fad4c2bb68d29
Mariona Estrada <165200714+estradapm@users.noreply.github.com>
2024-05-31 17:14:15 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let domain = [".com", ".es"];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let textHTML = '<ul class="list-group">';
+
+  for (let ipronoun = 0; ipronoun < pronoun.length; ipronoun++) {
+    for (let iadj = 0; iadj < adj.length; iadj++) {
+      for (let inoun = 0; inoun < noun.length; inoun++) {
+        for (let idomain = 0; idomain < domain.length; idomain++) {
+          let fullDomain =
+            pronoun[ipronoun] + adj[iadj] + noun[inoun] + domain[idomain];
+          textHTML += '<li class="list-group-item">' + fullDomain + "</li>";
+        }
+      }
+    }
+  }
+
+  textHTML += "</ul>";
+
+  let element = document.querySelector("#domain");
+  element.innerHTML = textHTML;
 };
